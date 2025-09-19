f616f56341577a2f38953506077297105543d0b2
watcharapong <watcharapong310545@gmail.com>
2025-09-19 15:45:11 +0700

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,15 @@
+public class Item {
+    private float price;
+
+    public Item() {
+        this.price = 0f;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float p) {
+        this.price = p;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.setPrice(99.5f);
+        System.out.println(item.getPrice());
+    }
+}
